9fd31cec57eb2620bb805e9ef77e49fa1410629b
Asif Anwar <asif.anwar@orbitax.com>
2024-12-12 11:39:42 +0600

    Add the data models

---
+++ StudentToDoApi/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace StudentToDoApi.Models
+{
+    public class Student
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonRequired]
+        public string Username { get; set; }
+
+        [BsonRequired]
+        public string PasswordHash { get; set; } // Password is hashed for security
+    }
+}
---
+++ StudentToDoApi/Models/ToDo.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace StudentToDoApi.Models
+{
+    public class ToDo
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonRequired]
+        public string StudentId { get; set; } // Links to the student
+        public string Task {  get; set; }
+        public bool IsCompleted { get; set; } = false;
+    }
+}
